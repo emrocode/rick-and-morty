760d7a3014df324f94f27e8252e8b4d8220b0adc
emrocode <92260849+emrocode@users.noreply.github.com>
2022-09-29 11:46:47 -0400

    Remove pagination

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,27 +1,21 @@
-import ReactPaginate from "react-paginate";
-
 export default function Navigation(props) {
-  const pageCount = 42;
-  const handlePageChange = (e) => props.setPage(e.selected + 1);
-
   return (
-    <header className="sticky top-0 z-10 flex h-24 select-none bg-white-100 shadow-sm">
+    <header className="sticky top-0 z-10 flex h-20 select-none bg-white-100 shadow-sm">
       <nav className="mx-auto flex w-[90%] max-w-[1440px] items-center justify-between">
         <span className="block text-sm font-bold text-black-300">
-          Page: {props.page} of {pageCount}
+          Page: {props.page} of 42
         </span>
-        {/* Mobile navigation */}
-        <div className="flex gap-x-2 sm:hidden">
+        <div className="flex gap-x-2">
           <button
-            className="relative inline-flex items-center rounded-full border bg-black-300 px-5 py-2 text-sm font-medium text-white-200"
+            className="relative inline-flex items-center rounded-md border bg-black-300 px-5 py-2 text-sm font-medium text-white-200"
             onClick={() =>
-              props.setPage((props.page > 1 && props.page - 1) || 1)
+              props.setPage((props.page >= 1 && props.page - 1) || 1)
             }
           >
             Previous
           </button>
           <button
-            className="relative inline-flex items-center rounded-full border bg-black-300 px-5 py-2 text-sm font-medium text-white-200"
+            className="relative inline-flex items-center rounded-md border bg-black-300 px-5 py-2 text-sm font-medium text-white-200"
             onClick={() =>
               props.setPage((props.page < 42 && props.page + 1) || 42)
             }
@@ -29,22 +23,6 @@ export default function Navigation(props) {
             Next
           </button>
         </div>
-        {/* Desktop pagination */}
-        <ReactPaginate
-          containerClassName="hidden md:inline-flex isolate -space-x-px rounded-sm shadow-sm"
-          pageCount={pageCount}
-          pageRangeDisplayed={3}
-          marginPagesDisplayed={2}
-          renderOnZeroPageCount={null}
-          onPageChange={handlePageChange}
-          pageClassName="inline-flex"
-          pageLinkClassName="relative inline-flex items-center border border-black-300 bg-black-100 px-4 py-3 text-sm font-medium text-white-200 focus:z-20"
-          previousLinkClassName="relative inline-flex items-center rounded-l-sm border border-black-300 bg-black-100 px-3 py-3 text-sm font-medium text-white-200 focus:z-20"
-          breakLabel="..."
-          activeLinkClassName="bg-black-200"
-          breakLinkClassName="relative inline-flex items-center border border-black-300 bg-black-100 px-4 py-3 text-sm font-medium text-white-200 focus:z-20"
-          nextLinkClassName="relative inline-flex items-center rounded-r-sm border border-black-300 bg-black-100 px-3 py-3 text-sm font-medium text-white-200 focus:z-20"
-        />
       </nav>
     </header>
   );
