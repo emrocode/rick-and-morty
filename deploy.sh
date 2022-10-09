9c38dc61c219bb4e56c338ec17861b1070811b4d
emrocode <92260849+emrocode@users.noreply.github.com>
2022-10-09 14:12:30 -0300

    Updated code

--- deploy.sh
+++ deploy.sh
@@ -9,14 +9,10 @@ npm run build
 # navigate into the build output directory
 cd dist
 
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
-
 git init
 git checkout -b main
 git add -A
 git commit -m 'deploy'
-
 git push -f git@github.com:emrocode/rick-and-morty.git main:gh-pages
 
 cd -
