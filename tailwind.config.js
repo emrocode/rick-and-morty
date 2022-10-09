26043f12e5fc216739dee77519b6604c76b73eea
emrocode <92260849+emrocode@users.noreply.github.com>
2022-10-09 14:12:47 -0300

    Updated code

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,16 +2,6 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      colors: {
-        "black-300": "#1F1F1F",
-        "black-200": "#333333",
-        "black-100": "#474747",
-        "white-300": "#C2C2C2",
-        "white-200": "#EBEBEB",
-        "white-100": "#FEFEFE",
-      },
-    },
     fontFamily: {
       sans: ["Open Sans", "sans-serif"],
     },
--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: 'https://emrocode.github.io/rick-and-morty/',
+  base: '/rick-and-morty/',
   plugins: [react()]
 })
